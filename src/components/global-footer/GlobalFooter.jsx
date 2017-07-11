efb92b6426aae43af4302a402dba5c7e1f39a645
Alexis Córdova <acordova@mac.com>
2017-07-11 14:28:56 -0700

    Add base global footer

--- src/components/global-footer/GlobalFooter.jsx
+++ src/components/global-footer/GlobalFooter.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import './style.scss'
 
 const GlobalFooter = () => {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className="row">
+      <div className="column-small-12">
+        <p className="u-text-center">Alexis Córdova <span className="icon-heart">♥</span> Krystle Mariano</p>
+      </div>
     </footer>
   )
 }
