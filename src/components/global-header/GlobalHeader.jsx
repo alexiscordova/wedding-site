ad0e6cd845a546c047e5d17a11b74e3d6cedb237
Alexis Córdova <acordova@mac.com>
2017-07-11 14:29:07 -0700

    Add base global header and navigation

--- src/components/global-header/GlobalHeader.jsx
+++ src/components/global-header/GlobalHeader.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import './style.scss'
 
 const GlobalHeader = () => {
   return (
-    <header>
-      <p>Header</p>
+    <header className="row">
+      <div className="header-logo column-small-2 column-medium-3">
+        <img src="http://placehold.it/85?text=Logo" />
+      </div>
+      <nav className="header-navigation column-small-10 column-medium-offset-2 column-medium-7">
+        <ul className="u-list-unstyled" role="navigation">
+          <li>
+            <a className="nav-link" href="#our-story">Our Story</a>
+          </li>
+          <li>
+            <a className="nav-link" href="#event-details">Event Details</a>
+          </li>
+          <li>
+            <a className="nav-link" href="#registry">Registry</a>
+          </li>
+          <li>
+            <a className="nav-link" href="#rsvp">RSVP</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
