90a816eafc1433014cda6329000a3148509eb641
Alexis Córdova <acordova@mac.com>
2017-07-12 15:32:39 -0700

    Add Picturefill

--- src/containers/app/App.jsx
+++ src/containers/app/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import GlobalHeader from 'Components/global-header/GlobalHeader'
 import GlobalFooter from 'Components/global-footer/GlobalFooter'
 import Home from 'Containers/home/Home'
+import 'picturefill'
+import 'picturefill/dist/plugins/mutation/pf.mutation.js'
 
 const App = () => {
   return (
