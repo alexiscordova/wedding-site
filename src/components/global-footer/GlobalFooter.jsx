765df67466ee185996a2837d1d2da90eb6fcbc97
Alexis Córdova <acordova@mac.com>
2017-07-12 15:37:12 -0700

    Restructure GlobalFooter

--- src/components/global-footer/GlobalFooter.jsx
+++ src/components/global-footer/GlobalFooter.jsx
@@ -3,9 +3,11 @@ import './style.scss'
 
 const GlobalFooter = () => {
   return (
-    <footer className="row">
-      <div className="column-small-12">
-        <p className="u-text-center">Alexis Córdova <span className="icon-heart">♥</span> Krystle Mariano</p>
+    <footer>
+      <div className="row">
+        <div className="column-small-12">
+          <p className="u-text-center">Alexis Córdova <span className="icon-heart">♥</span> Krystle Mariano</p>
+        </div>
       </div>
     </footer>
   )
