5697096e5976bc420fa73d3af956b805af6f5967
Alexis Córdova <acordova@mac.com>
2017-08-09 10:40:58 -0700

    Update CSS/SCSS StyleLint rule definitions

--- stylelint.config.js
+++ stylelint.config.js
@@ -8,6 +8,10 @@ module.exports = {
   'rules': {
     'selector-pseudo-element-colon-notation': null,
     'number-no-trailing-zeros': null,
-    'at-rule-empty-line-before': null
+    'at-rule-empty-line-before': null,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': [true, {
+      'ignoreAtRules': ['/^import-/']
+    }]
   }
 }
