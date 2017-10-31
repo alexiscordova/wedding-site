076112ac330262478bf720ede959e1ade1750a1a
Alexis Córdova <acordova@mac.com>
2017-10-31 10:08:56 -0700

    Initialize Google Analytics

--- src/containers/app/App.jsx
+++ src/containers/app/App.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ReactGA from 'react-ga'
 import GlobalHeader from 'Components/global-header/GlobalHeader'
 import GlobalFooter from 'Components/global-footer/GlobalFooter'
 import Home from 'Containers/home/Home'
 import 'picturefill'
 import 'picturefill/dist/plugins/mutation/pf.mutation.js'
 
-const App = () => {
-  return (
-    <article className="react-root">
-      <GlobalHeader />
+ReactGA.initialize('UA-347921-7')
 
-      <main>
-        <Home />
-      </main>
+class App extends Component {
+  constructor(props) {
+    super(props)
 
-      <GlobalFooter />
-    </article>
-  )
+    ReactGA.set({
+      page: window.location.href
+    })
+
+    ReactGA.pageview(window.location.href)
+  }
+  render() {
+    return (
+      <article className="react-root">
+        <GlobalHeader />
+
+        <main>
+          <Home />
+        </main>
+
+        <GlobalFooter />
+      </article>
+    )
+  }
 }
 
 export default App
